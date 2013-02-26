d55604c6fa0595f0bda4e1c5b99c34afcd576fa6
Derek Salama <derek.salama@originate.com>
2013-02-26 07:07:48 -0800

    array of invites

--- src/com/foodcirclesserver/circles/SendInviteServlet.java
+++ src/com/foodcirclesserver/circles/SendInviteServlet.java
@@ -1,5 +1,7 @@
 package com.foodcirclesserver.circles;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,29 +18,31 @@ public class SendInviteServlet extends HttpServlet {
 	private static final long serialVersionUID = -5354835351160362455L;
 
 	//Should this be POST?
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		String circleIDString = req.getParameter(InviteManager.CIRCLE_ID);
 		Long circleID = Long.parseLong(circleIDString);
 
 		String senderID = req.getParameter(InviteManager.SENDER_ID);
-		String receiverID = req.getParameter(InviteManager.RECEIVER_ID);
+		String[] receiverIDs = req.getParameterValues(InviteManager.RECEIVER_ID);
 
-		if (circleID == null || senderID == null || receiverID == null)
-			return;
+		if (circleID == null || senderID == null || receiverIDs == null)
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
 
 		String tokenHash = (req.getParameter(UserManager.TOKEN_HASH));
 		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
 
 		try {
 			if (UserManager.validateUser(tokenHash, senderID, ds)) {
-			Key k = InviteManager.saveInvite(circleID, senderID, receiverID, ds);
-				//TODO: send notification!
-		//display user name and circle name on invite
-		//use key to accept invite
+				for(String receiver : receiverIDs) {
+					//TODO: send notification!
+					Key k = InviteManager.saveInvite(circleID, senderID, receiver, ds);
+				}
+				resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+				//display user name and circle name on invite
+				//use key to accept invite
 			}
 		} catch (EntityNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
 		}
 
 
